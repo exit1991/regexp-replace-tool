ed3c0d1caae015c7610f5375bd088e4ba6102ed5
hdeg <76611318+exit1991@users.noreply.github.com>
2021-12-11 23:25:36 +0900

    検索ハイライト機能追加

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -5,10 +5,29 @@ const elem = {
      'before-text':         document.querySelector('#before-text')
     ,'search-text':         document.querySelector('#search-text')
     ,'replace-text':        document.querySelector('#replace-text')
+    ,'searched-text':       document.querySelector('#searched-text')
     ,'after-text':          document.querySelector('#after-text')
+    ,'exec-search-button':  document.querySelector('#exec-search-button')
     ,'exec-replace-button': document.querySelector('#exec-replace-button')
     ,'result-clear-button': document.querySelector('#result-clear-button')
 }
+
+// 検索ボタンがクリックした際の処理
+elem['exec-search-button'].addEventListener('click', () => {
+    // 入力値の取得
+    const targetBeforeStr = elem['before-text'].value;
+    const searchStr = '(' + elem['search-text'].value + ')';
+    const replaceStr = '<mark>$1</mark>';
+    
+    // RegExp 第2引数  g : 同じ行で2回以上ヒットさせる
+    //                 m : 複数行で、それぞれの行に対してヒットさせる
+    const searchRegExp = new RegExp(searchStr, 'gm');
+    const targetAfterStr = targetBeforeStr.replace(searchRegExp, replaceStr);
+    
+    // 検索後のセット
+    elem['searched-text'].innerHTML = targetAfterStr.replaceAll('\n','<BR>');
+    console.log(targetAfterStr);
+});
 
 // 実行ボタンがクリックした際の処理
 elem['exec-replace-button'].addEventListener('click', () => {
@@ -28,5 +47,6 @@ elem['exec-replace-button'].addEventListener('click', () => {
 
 // 結果クリアボタンがクリックした際の処理
 elem['result-clear-button'].addEventListener('click', () => {
+    elem['searched-text'].innerHTML = '';
     elem['after-text'].value = '';
 });
